4df0454120d146b65a72f45564b26a18888caacb
Leead Jacobowitz <leead123@gmail.com>
2023-07-24 11:52:36 +0300

    Initial commit + added splash page

---
+++ app/src/main/java/com/example/ptolemycoinclassifier/splash_screen.java
@@ -0,0 +1,40 @@
+package com.example.ptolemycoinclassifier;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class splash_screen extends AppCompatActivity {
+    Handler h = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN , WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Remove the title from the ActionBar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("");
+        }
+
+        actionBar.hide();
+
+        setContentView(R.layout.activity_splash_screen);
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash_screen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
